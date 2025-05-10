70bfcc58c9049a499c59f1d4ae460ed9fa785452
retro.dev <117005347+ay30bg@users.noreply.github.com>
2025-05-10 09:46:27 +0100

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -42,7 +42,6 @@ function Login() {
   };
 
   return (
-     <AdminAuthHeader />
     <div className="login-page container">
       <AdminAuthHeader />
       <h1 className="login-header">Admin Login</h1>
