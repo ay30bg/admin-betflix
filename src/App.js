11d636a00e9c9e212934bbe2d06306e3512a6758
retro.dev <117005347+ay30bg@users.noreply.github.com>
2025-05-10 08:43:20 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import Login from './components/Login';
-import SignUp from './components/SignUp';
 import Rounds from './components/Rounds';
 import Users from './components/Users';
 import Transactions from './components/Transactions';
 import MainContent from './components/MainContent';
 import ActivityLogs from './ActivityLogs';
 import SetRoundOutcome from './components/SetRoundOutcome';
+import Signup from './components/SignUp';
 import './App.css';
 
 function App() {
@@ -18,7 +18,7 @@ function App() {
         <Routes>
           {/* Login route without Dashboard/sidebar */}
           <Route path="/" element={<Login />} />
-          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/sign-up" element={<Signup />} />
           
           {/* Dashboard route with nested routes */}
           <Route path="dashboard" element={<Dashboard />}>
