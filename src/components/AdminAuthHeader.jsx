8eb455596e3ec922ed4f043bc51fbad5b28ad0f4
retro.dev <117005347+ay30bg@users.noreply.github.com>
2025-05-10 10:25:48 +0100

    Update AdminAuthHeader.jsx

--- src/components/AdminAuthHeader.jsx
+++ src/components/AdminAuthHeader.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 // import { useAuth } from '../context/AuthContext';
-import '../styles/header.css',
+import '../styles/header.css',;
 
 function AdminAuthHeader() {
   // const { logout } = useAuth();
