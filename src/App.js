0693e4532e72380ee63f2aef6307ea5b86cd5a33
retro.dev <117005347+ay30bg@users.noreply.github.com>
2025-05-10 08:36:06 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,8 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Dashboard from './components/Dashboard';
-// import Login from './components/Login';
-// import Rounds from './components/Rounds';
-// import Users from './components/Users';
-// import Transactions from './components/Transactions';
-// import MainContent from './components/MainContent';
-// import ActivityLogs from './ActivityLogs';
-// import SetRoundOutcome from './components/SetRoundOutcome';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <Router>
-//       <div className="App">
-//         <Routes>
-//           <Route path="/" element={<Dashboard />}>
-//             <Route index element={<MainContent />} />
-//             <Route path="rounds" element={<Rounds />} />
-//             <Route path="users" element={<Users />} />
-//             <Route path="transactions" element={<Transactions />} />
-//             <Route path="activity" element={<ActivityLogs />} />
-//             <Route path="set-round-outcome" element={<SetRoundOutcome />} />
-//             <Route path="login" element={<Login />} />
-//           </Route>
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import Login from './components/Login';
+import SignUp from './components/SignUp';
 import Rounds from './components/Rounds';
 import Users from './components/Users';
 import Transactions from './components/Transactions';
@@ -51,6 +18,7 @@ function App() {
         <Routes>
           {/* Login route without Dashboard/sidebar */}
           <Route path="/" element={<Login />} />
+          <Route path="/sign-up" element={<Login />} />
           
           {/* Dashboard route with nested routes */}
           <Route path="dashboard" element={<Dashboard />}>
