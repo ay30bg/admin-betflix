b9b429795416a23c750083e7238ac2809dbc7cf3
retro.dev <117005347+ay30bg@users.noreply.github.com>
2025-05-15 10:29:43 +0100

    Update ResetPassword.jsx

--- src/components/ResetPassword.jsx
+++ src/components/ResetPassword.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';;
-import '../styles/login.css';
 
 function ResetPassword() {
   const navigate = useNavigate();
