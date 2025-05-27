8c77c9b9b62af1de95333bf1e398f9d8855c46f8
retro.dev <117005347+ay30bg@users.noreply.github.com>
2025-05-27 20:01:31 +0100

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { FiHome, FiUsers, FiDollarSign, FiBarChart2, FiLayers, FiCreditCard, FiPhone, FiActivity, FiEnvelope  } from 'react-icons/fi';
+import { FiHome, FiUsers, FiDollarSign, FiBarChart2, FiLayers, FiCreditCard, FiPhone, FiActivity, FiMail   } from 'react-icons/fi';
 
 const Sidebar = () => {
   return (
@@ -78,7 +78,7 @@ const Sidebar = () => {
             to="/dashboard/redenvelope" // Updated path
             className={({ isActive }) => `sidebar-item ${isActive ? 'active' : ''}`}
           >
-            <FiEnvelope className="sidebar-icon" />
+            <FiMail className="sidebar-icon" />
             <span>Red Envelope</span>
           </NavLink>
         </li>
